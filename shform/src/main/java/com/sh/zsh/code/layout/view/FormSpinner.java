be271e735d2689b6dd1ee59687f30fdb853492ed
beiguancyc <9766@163.com>
2018-05-03 21:15:24 +0800

    把spinner集成到原目录中去

--- shform/src/main/java/com/sh/zsh/code/layout/view/FormSpinner.java
+++ shform/src/main/java/com/sh/zsh/code/layout/view/FormSpinner.java
@@ -1,23 +1,25 @@
 package com.sh.zsh.code.layout.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.TextView;
+import android.view.inputmethod.InputMethodManager;
 
 import com.bigkoo.pickerview.OptionsPickerView;
-import com.sh.shform.R;
 import com.sh.zsh.code.layout.model.ViewData;
 
 import java.util.ArrayList;
+
 
 /**
  * Created by zhush on 2017/3/14.
  * E-mail 405086805@qq.com
  * PS
  */
-public class FormSpinner extends TextView{
+public class FormSpinner extends AppCompatTextView {
 
     OptionsPickerView pvOptions;//选项选择器
     Object pvOptionsSelectValue;//选中的value
@@ -37,20 +39,26 @@ public class FormSpinner extends TextView{
         });
     }
     int selectIndex;
-    public FormSpinner(Context context, AttributeSet attrs) {
+    public FormSpinner(final Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.less_from_view, 0, 0);
-        selectIndex = ta.getInt(R.styleable.less_from_view_less_form_spi_sel,999);
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, com.sh.shform.R.styleable.less_from_view, 0, 0);
+        selectIndex = ta.getInt(com.sh.shform.R.styleable.less_from_view_less_form_spi_sel,999);
         pvOptions = new OptionsPickerView(context);
 
         setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                Activity activity=(Activity) context;
+                InputMethodManager imm =  (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                if(imm != null) {
+                    imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(),
+                            0);
+                }
                 pvOptions.show();
             }
         });
     }
-    public void setpvOptionsList(ArrayList<ViewData> options,String titel){
+    public void setpvOptionsList(ArrayList<ViewData> options, String titel){
 
         this.titel =titel;
         setpvOptionsList(options);
@@ -64,9 +72,14 @@ public class FormSpinner extends TextView{
 
         //三级联动效果
         pvOptions.setPicker(options1Items);
+
+        //pvOptions.setPicker(options1Items,options1Items,true);
         //设置选择的三级单位
-//        pwOptions.setLabels("省", "市", "区");
+        //pvOptions.setLabels("省", "市");
+
+
         pvOptions.setTitle(titel);
+        super.setText(titel);
         pvOptions.setCyclic(false, true, true);
         //设置默认选中的三级项目
         //监听确定选择按钮
@@ -75,12 +88,11 @@ public class FormSpinner extends TextView{
 
             @Override
             public void onOptionsSelect(int options1, int option2, int options3) {
-
+                //返回的分别是三个级别的选中位置
                 if(isinit){
                     options1=selectIndex;
                     isinit=false;
                 }
-                //返回的分别是三个级别的选中位置
                 String tx = options1Items.get(options1).getPickerViewText();
                 FormSpinner.super.setText(tx);
                 pvOptionsSelectValue = options1Items.get(options1).getValue();
@@ -88,14 +100,12 @@ public class FormSpinner extends TextView{
                 if(selectedListener!=null){
                     selectedListener.pvOptions(tx,pvOptionsSelectValue,pvOptionsSelectValueObject);
                 }
-
-
             }
         });
 
         if(selectIndex>=0&&selectIndex<options.size()){
             //默认选中第一个
-            pvOptions.setSelecedItem(selectIndex);
+            pvOptions.setSelecedItem(selectIndex,0,0);
         }
 
 
@@ -105,7 +115,7 @@ public class FormSpinner extends TextView{
      * 选中监听
      */
     public static interface SelectedListener{
-        public void pvOptions(String key, Object value,Object pvOptionsSelectValueObject);
+        public void pvOptions(String key, Object value, Object pvOptionsSelectValueObject);
     }
 
     public void setSelectedListener(SelectedListener selectedListener) {
